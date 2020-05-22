efb2f8791006fa5cb25c5e1de3de604b712283cc
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2020-05-22 15:02:14 -0700

    Update J-Bdork.py

--- J-Bdork.py
+++ J-Bdork.py
@@ -21,9 +21,13 @@ from datetime import datetime
 from os.path import isfile
 from os.path import isdir
 from os import mkdir
-from fake_useragent import UserAgent
 
 class jancok_dorker:
+
+    def useragent(self):
+
+        arr=["Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12 (.NET CLR 3.5.30729)","Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.464.0 Safari/534.3","Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10_5_8; ja-jp) AppleWebKit/533.16 (KHTML, like Gecko) Version/5.0 Safari/533.16","Mozilla/5.0 (X11; U; FreeBSD i386; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/4.0.207.0 Safari/532.0","Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/534.1 (KHTML, like Gecko) Chrome/6.0.427.0 Safari/534.1"]
+        return arr[randint(0,len(arr)-1)]
 
     def check_site(self, site):
 
@@ -45,7 +49,7 @@ class jancok_dorker:
 
         try:
 
-            resp = get(url="https://www.bing.com/search?q={}&first={}".format(dork, str(page)), headers={ "User-Agent": UserAgent().random }, timeout=5)
+            resp = get(url="https://www.bing.com/search?q={}&first={}".format(dork, str(page)), headers={ "User-Agent": self.useragent() }, timeout=5)
             link = findall("href=\"(.+?)\"", resp.text)
 
             for x in link:
